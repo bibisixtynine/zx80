59c9007666678afa5f101cdeacadec12732f66a3
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2023-01-10 23:50:36 +0100

    add .env

--- api/hand.js
+++ api/hand.js
@@ -1,7 +1,19 @@
+// this is a serverless function
+//
+// you can read more about serverless functions here: https://vercel.com/docs/serverless-functions/introduction
+//
+// -> to test this api you can browse https://WEBSITE_URL/api/hand
+//
+// -> during dev, use terminal command: vercel dev
+// (you can't access the api with terminal command : vite dev)
+//
+// => your will need vercel cli installed: https://vercel.com/download
+
 export default function handler(request, response) {
     response.status('200').json({
       body: "so nice",//request.body,
       query: "hello wold",//request.query,
       cookies: "so tasty",//request.cookies,
+      secret: `${process.env.SECRET_KEY}`
     });
   }
--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,30 @@
+// this is an edge function
+//
+// it's a serverless function that runs on the edge (faster, but more limited)
+//
+// you can read more about edge functions here: https://vercel.com/docs/concepts/functions/edge-functions
+//
+// -> to test this api you can browse https://WEBSITE_URL/api/hello
+//
+// -> during dev, use terminal command: vercel dev
+// (you can't access the api with terminal command : vite dev)
+//
+// => your will need vercel cli installed: https://vercel.com/download
+
+
 export const config = {
     runtime: 'edge', // this is a pre-requisite
     regions: ['cdg1'], // only execute this function on cdg1
 };
 
+
 export default (req) => {
-    return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
+    debugger
+    return new Response(`
+    🤩 Hello from ${req.url} !
+    🔥 I'm now an Edge Function!
+    🔐 I can use the secret key: ${process.env.SECRET_KEY}
+
+    💫😎🚀
+    `);
 };
